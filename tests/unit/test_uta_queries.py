4b40977e47be09f0ec02cbb7d86b720de9278d72
korikuzma <korikuzma@gmail.com>
2021-09-29 09:54:41 -0400

    Add more tests

--- tests/unit/test_uta_queries.py
+++ tests/unit/test_uta_queries.py
@@ -1,4 +1,5 @@
 """Test UTA data source."""
+from typing import List, Optional
 import pytest
 from uta_tools.uta import UTADatabase
 import copy
@@ -19,6 +20,31 @@ async def test_db():
             return await self.test_db.transcript_to_genomic(
                 tx_ac, start_exon, end_exon, start_exon_offset,
                 end_exon_offset, gene)
+
+        async def get_tx_exons(self, tx_ac):
+            return await self.test_db.get_tx_exons(tx_ac)
+
+        async def get_tx_exon_start_end(
+                self, tx_ac: str, exon_start: int, exon_end: int):
+            return await self.test_db.get_tx_exon_start_end(
+                tx_ac, exon_start, exon_end)
+
+        def get_tx_exon_coords(self, tx_exons: List[str],
+                               exon_start: int, exon_end: int):
+            return self.test_db.get_tx_exon_coords(
+                tx_exons, exon_start, exon_end)
+
+        async def get_alt_ac_start_and_end(
+                self, tx_ac: str, tx_exon_start: List[str],
+                tx_exon_end: List[str], gene: str = None):
+            return await self.test_db.get_alt_ac_start_and_end(
+                tx_ac, tx_exon_start, tx_exon_end, gene)
+
+        async def get_alt_ac_start_or_end(
+                self, tx_ac: str, tx_exon_start: int,
+                tx_exon_end: int, gene: Optional[str] = None):
+            return await self.test_db.get_alt_ac_start_or_end(
+                tx_ac, tx_exon_start, tx_exon_end, gene)
     return TestUTADatabase()
 
 
@@ -50,6 +76,82 @@ def ntrk1_exon10_exon17():
         "exon_end_offset": 0,
         "exon_start_offset": 0
     }
+
+
+@pytest.fixture(scope='module')
+def nm_152263_exons():
+    """Create test fixture for NM_152263.3 exons."""
+    return ["117,234", "234,360", "360,494", "494,612", "612,683", "683,759",
+            "759,822", "822,892", "892,971", "971,975"]
+
+
+@pytest.fixture(scope='module')
+def tpm3_1_8_start_genomic():
+    """Create test fixture for genomic data for exon 1, 8"""
+    return 'TPM3', 'NC_000001.11', 154191901, 154192135, -1
+
+
+@pytest.fixture(scope='module')
+def tpm3_1_8_end_genomic():
+    """Create test fixture for genomic data for exon 1, 8"""
+    return 'TPM3', 'NC_000001.11', 154170399, 154170469, -1
+
+
+@pytest.mark.asyncio
+async def test_get_tx_exons(test_db, nm_152263_exons):
+    """Test that get_tx_exons works correctly."""
+    resp = await test_db.get_tx_exons('NM_152263.3')
+    assert resp == nm_152263_exons
+
+    # Invalid transcript accession
+    resp = await test_db.get_tx_exons('NM_152263.36')
+    assert resp is None
+
+
+@pytest.mark.asyncio
+async def test_get_tx_exon_start_end(test_db, nm_152263_exons):
+    """Test that get_tx_exon_start_end works correctly."""
+    resp = await test_db.get_tx_exon_start_end('NM_152263.3', 1, 8)
+    assert resp == (nm_152263_exons, 1, 8)
+
+    resp = await test_db.get_tx_exon_start_end('NM_152263.3', 0, 8)
+    assert resp == (nm_152263_exons, 1, 8)
+
+    resp = await test_db.get_tx_exon_start_end('NM_152263.3', 0, 0)
+    assert resp == (nm_152263_exons, 1, 10)
+
+    resp = await test_db.get_tx_exon_start_end('NM_152263.3', 8, 1)
+    assert resp is None
+
+
+@pytest.mark.asyncio
+async def test_get_tx_exon_coords(test_db, nm_152263_exons):
+    """Test that get_tx_exon_coords works correctly."""
+    resp = test_db.get_tx_exon_coords(nm_152263_exons, 1, 8)
+    assert resp == (["117", "234"], ["822", "892"])
+
+    resp = test_db.get_tx_exon_coords(nm_152263_exons, 1, 11)
+    assert resp is None
+
+
+@pytest.mark.asyncio
+async def test_get_alt_ac_start_and_end(test_db, tpm3_1_8_start_genomic,
+                                        tpm3_1_8_end_genomic):
+    """Test that get_alt_ac_start_and_end works correctly."""
+    resp = await test_db.get_alt_ac_start_and_end(
+        'NM_152263.3', ["117", "234"], ["822", "892"], "TPM3")
+    assert resp == (tpm3_1_8_start_genomic, tpm3_1_8_end_genomic)
+
+
+@pytest.mark.asyncio
+async def test_get_alt_ac_start_or_end(test_db, tpm3_1_8_start_genomic,
+                                       tpm3_1_8_end_genomic):
+    """Test that get_alt_ac_start_or_end works correctly."""
+    resp = await test_db.get_alt_ac_start_or_end('NM_152263.3', 117, 234)
+    assert resp == tpm3_1_8_start_genomic
+
+    resp = await test_db.get_alt_ac_start_or_end('NM_152263.3', 822, 892)
+    assert resp == tpm3_1_8_end_genomic
 
 
 @pytest.mark.asyncio
