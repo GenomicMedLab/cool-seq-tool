1054fd0a27de97a9765953249e5d780f590c3382
korikuzma <korikuzma@gmail.com>
2021-10-09 19:44:14 -0400

    Update schemas to reflect optional exons

--- uta_tools/schemas.py
+++ uta_tools/schemas.py
@@ -1,8 +1,8 @@
 """Module for data models."""
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, root_validator
 from pydantic.main import Extra
-from typing import Literal
+from typing import Literal, Optional
 
 
 class ResidueMode(str, Enum):
@@ -24,11 +24,27 @@ class GenomicData(BaseModel):
     chr: str
     start: int  # Genomic start position
     end: int  # Genomic end position
-    exon_start: int
-    exon_start_offset: int = 0
-    exon_end: int
-    exon_end_offset: int = 0
+    exon_start: Optional[int] = None
+    exon_start_offset: Optional[int] = 0
+    exon_end: Optional[int] = None
+    exon_end_offset: Optional[int] = 0
     transcript: str
+
+    @root_validator(pre=True)
+    def check_exons(cls, values):
+        """Check that at least one of {`exon_start`, `exon_end`} is set.
+        If not set, set corresponding offset to `None`
+        """
+        msg = "Must give values for either `exon_start`, `exon_end`, or both"
+        exon_start = values.get("exon_start")
+        exon_end = values.get("exon_end")
+        assert exon_start or exon_end, msg
+
+        for exon, exon_offset in [(exon_start, "exon_start_offset"),
+                                  (exon_end, "exon_end_offset")]:
+            if exon is None:
+                values[exon_offset] = None
+        return values
 
 
 class TranscriptExonData(BaseModel):
