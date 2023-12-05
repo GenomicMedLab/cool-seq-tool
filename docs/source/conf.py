a2c0c458aef5cd5bf1f385bc124c93ac5ee0129a
James Stevenson <james.stevenson@nationwidechildrens.org>
2023-12-05 07:52:13 -0500

    add boilerplate

---
+++ docs/source/conf.py
@@ -0,0 +1,85 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "cool-seq-tool"
+copyright = "2023, Wagner Lab"
+author = "Wagner Lab"
+html_title = "cool-seq-tool"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_autodoc_typehints",
+    "sphinx.ext.linkcode",
+    "sphinx_copybutton",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = []
+html_css_files = [
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/fontawesome.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/solid.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/brands.min.css",
+]
+html_theme_options = {
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/cancervariants/gene-normalization",
+            "html": "",
+            "class": "fa-brands fa-solid fa-github",
+        },
+        {
+            "name": "Wagner Lab",
+            "url": "https://www.nationwidechildrens.org/specialties/institute-for-genomic-medicine/research-labs/wagner-lab",
+            "html": "",
+            "class": "fa-solid fa-house",
+        },
+        {
+            "name": "Twitter",
+            "url": "https://twitter.com/genomicmedlab",
+            "html": "",
+            "class": "fa-solid fa-brands fa-twitter",
+        },
+    ],
+}
+# -- autodoc things ----------------------------------------------------------
+import os  # noqa: E402
+import sys  # noqa: E402
+
+sys.path.insert(0, os.path.abspath("../../cool_seq_tool"))
+autodoc_preserve_defaults = True
+
+# -- get version -------------------------------------------------------------
+from cool_seq_tool.version import __version__  # noqa: E402
+
+version = __version__
+release = version
+
+
+# -- linkcode ----------------------------------------------------------------
+def linkcode_resolve(domain, info):
+    if domain != "py":
+        return None
+    if not info["module"]:
+        return None
+    filename = info["module"].replace(".", "/")
+    return f"https://github.com/genomicmedlab/cool-seq-tool/blob/main/{filename}.py"  # noqa: E501
+
+
+# -- code block style --------------------------------------------------------
+pygments_style = "default"
+pygements_dark_style = "monokai"
