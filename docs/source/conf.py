22954bde25aa0bd9819b38e09825a81172116290
James Stevenson <james.stevenson@nationwidechildrens.org>
2023-12-22 14:33:03 -0500

    intentionally introduce an error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+from sdflkj import sdlfk
 project = "Cool-Seq-Tool"
 copyright = "2023, Wagner Lab"
 author = "Wagner Lab"
