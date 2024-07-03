49bd762d97485d0ca67db071cddb57321d9e4748
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-07-03 08:30:15 -0400

    feat!: use setuptools-scm, capture version at package root (#301)

--- src/cool_seq_tool/__init__.py
+++ src/cool_seq_tool/__init__.py
@@ -1,6 +1,7 @@
 """The cool_seq_tool package"""
 
 import logging
+from importlib.metadata import PackageNotFoundError, version
 
 logging.basicConfig(
     filename="cool_seq_tool.log",
@@ -10,3 +11,11 @@ logger = logging.getLogger("cool_seq_tool")
 logger.setLevel(logging.DEBUG)
 
 LOG_FN = "cool_seq_tool.log"
+
+
+try:
+    __version__ = version("cool_seq_tool")
+except PackageNotFoundError:
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
--- src/cool_seq_tool/version.py
+++
@@ -1,3 +0,0 @@
-"""Define package version."""
-
-__version__ = "0.4.1"
