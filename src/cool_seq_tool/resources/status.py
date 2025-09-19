31b379be4090a5f090a0db907db01c1a4a98ed34
Zeal Wierslee <zeal.wierslee@nationwidechildrens.org>
2025-09-19 13:25:33 -0400

    fix: Redact auth from DB url in status check (#431)
    At present, the status check logs the DB URL, including the password.
    
    This PR uses a helper function from pip to redact the password from the
    URLs, making them safe to log.
    
    After this change, this is what the log messages look like:
    ```
    ERROR:cool_seq_tool.resources.status:Encountered error instantiating UTA at URI postgresql://uta_admin:****@localhost:5432/uta/uta_20241220
    ```
    
    Closes #429
    
    ---------
    
    Co-authored-by: Kori Kuzma <korikuzma@gmail.com>
    Co-authored-by: jarbesfeld <jarbesfeld@gmail.com>
    Co-authored-by: Katie Stahl <kathryn.stahl@nationwidechildrens.org>

--- src/cool_seq_tool/resources/status.py
+++ src/cool_seq_tool/resources/status.py
@@ -3,6 +3,7 @@
 import logging
 from collections import namedtuple
 from pathlib import Path
+from urllib.parse import urlparse
 
 from agct._core import ChainfileError
 from asyncpg import InvalidCatalogNameError, UndefinedTableError
@@ -11,7 +12,7 @@ from biocommons.seqrepo import SeqRepo
 from cool_seq_tool.handlers.seqrepo_access import SEQREPO_ROOT_DIR, SeqRepoAccess
 from cool_seq_tool.mappers.liftover import LiftOver
 from cool_seq_tool.resources.data_files import DataFile, get_data_file
-from cool_seq_tool.sources.uta_database import UTA_DB_URL, UtaDatabase
+from cool_seq_tool.sources.uta_database import UTA_DB_URL, ParseResult, UtaDatabase
 
 _logger = logging.getLogger(__name__)
 
@@ -119,14 +120,20 @@ async def check_status(
     else:
         status["liftover"] = True
 
+    parsed_result = ParseResult(urlparse(db_url))
+    sanitized_url = parsed_result.sanitized_url
     try:
         await UtaDatabase.create(db_url)
+    except ValueError:
+        _logger.exception("Database URL is not valid")
     except (OSError, InvalidCatalogNameError, UndefinedTableError):
-        _logger.exception("Encountered error instantiating UTA at URI %s", UTA_DB_URL)
+        _logger.exception(
+            "Encountered error instantiating UTA at URI %s", sanitized_url
+        )
     except Exception as e:
         _logger.critical(
             "Encountered unexpected error instantiating UTA from URI %s: %s",
-            UTA_DB_URL,
+            sanitized_url,
             e,
         )
     else:
