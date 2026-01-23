7e3a95dec6840569aa81e22b13126514988f42e8
Jeremy Arbesfeld <50678786+jarbesfeld@users.noreply.github.com>
2026-01-23 10:58:21 -0500

    fix: Sort transcript data output (#452)
    closes #451

--- src/cool_seq_tool/sources/uta_database.py
+++ src/cool_seq_tool/sources/uta_database.py
@@ -543,7 +543,8 @@ class UtaDatabase:
         self, ac: str, alt_ac: str | None, start_pos: int, end_pos: int
     ) -> GenomicTxMetadata | None:
         """Get MANE transcript and genomic data. Used when going from g. to MANE c.
-        representation.
+        representation. This function parses queried data from the tx_exon_aln_v
+        table, and sorts the queried data by the most recent genomic build
 
         >>> import asyncio
         >>> from cool_seq_tool.sources import UtaDatabase
@@ -569,11 +570,17 @@ class UtaDatabase:
             successful
         """
         results = await self.get_tx_exon_aln_v_data(
-            ac, start_pos, end_pos, alt_ac=alt_ac, use_tx_pos=False
+            tx_ac=ac,
+            start_pos=start_pos,
+            end_pos=end_pos,
+            alt_ac=alt_ac,
+            use_tx_pos=False,
         )
         if not results:
             return None
-        result = results[0]
+
+        # Sort by most recent chromosomal accession
+        result = results[-1]
 
         genomic_tx_data = self.data_from_result(result)
         if not genomic_tx_data:
